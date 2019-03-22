77ed316fba481002ccf5985f41bd2bdb877aff30
Zein Ersyad <zeinersyad@gmail.com>
2019-03-22 19:55:00 +0700

    revisi auth header

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorisation"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   next();
 });
