3744bb550798b51af9eec64891a26087b776e555
Zein Ersyad <zeinersyad@gmail.com>
2019-03-30 13:56:45 +0700

    update gitignore

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -48,7 +48,7 @@ checkToken = (req, res, next) => {
 
 checkTokenAdmin = (req, res, next) => {
   console.log(req.headers)
-  let token = req.headers["authorization"]; // Express headers are auto converted to lowercase
+  let token = req.headers["x-access-token"] || req.headers["authorization"]; // Express headers are auto converted to lowercase
   if (token && token.startsWith("Bearer ")) {
     // Remove Bearer from string
     token = token.slice(7, token.length);
